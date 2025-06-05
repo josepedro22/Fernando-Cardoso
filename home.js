2853cf23c46a3a582d2ee00754f6752ee01145cd
José Pedro Araújo <josepedroaraujo@outlook.pt>
2025-06-05 19:26:56 +0100

    Mudar tudo

--- home.js
+++ home.js
@@ -1,37 +1,31 @@
 // Fechar o menu ao clicar fora dele
-  document.addEventListener('click', function (event) {
-    const navbarToggler = document.querySelector('.navbar-toggler');
-    const navbarCollapse = document.querySelector('#navbarNav');
+document.addEventListener('click', function (event) {
+  const navbarToggler = document.querySelector('.navbar-toggler');
+  const navbarCollapse = document.querySelector('#navbarNav');
 
-    // Verifica se o menu está aberto e o clique não foi dentro do menu ou no botão de toggle
-    if (navbarCollapse.classList.contains('show') &&
-        !navbarCollapse.contains(event.target) &&
-        !navbarToggler.contains(event.target)) {
-        // Fecha o menu
-        const collapseInstance = bootstrap.Collapse.getInstance(navbarCollapse);
-        collapseInstance.hide();
-    }
+  // Verifica se o menu está aberto e o clique não foi dentro do menu ou no botão de toggle
+  if (navbarCollapse.classList.contains('show') &&
+      !navbarCollapse.contains(event.target) &&
+      !navbarToggler.contains(event.target)) {
+      // Fecha o menu
+      const collapseInstance = bootstrap.Collapse.getInstance(navbarCollapse);
+      collapseInstance.hide();
+  }
 });
 
+//Função para não ficar nada por baixo da Navbar
 document.addEventListener("DOMContentLoaded", function () {
   const navbarHeight = document.querySelector(".navbar").offsetHeight;
   document.body.style.paddingTop = navbarHeight + "px";
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Selecionar o carrossel do Bootstrap
-  const testimonialCarousel = new bootstrap.Carousel("#testimonialCarousel");
-
-  // Botões de controle
-  const prevBtn = document.getElementById("prevBtn");
-  const nextBtn = document.getElementById("nextBtn");
-
-  // Adicionar eventos de clique
-  prevBtn.addEventListener("click", function () {
-    testimonialCarousel.prev(); // Vai para o slide anterior
-  });
-
-  nextBtn.addEventListener("click", function () {
-    testimonialCarousel.next(); // Vai para o próximo slide
+// Scroll suave para âncoras
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
   });
 });
+
